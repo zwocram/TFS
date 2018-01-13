74ae6f6113986cacb7caaa60a176970bd9e274ca
Marco van der Zwan <mnzwan@xs4all.nl>
2018-01-13 09:47:21 +0100

    Introducing custom made exceptions.

---
+++ tfs/ib/ibexceptions.py
@@ -0,0 +1,22 @@
+class ResolveContractDetailsException(Exception):
+    pass
+
+
+class IBTimeoutException(Exception):
+    pass
+
+
+class UnresolvedContractException(Exception):
+    pass
+
+
+class MultipleContractException(Exception):
+    pass
+
+
+class HistoricalDataRetrieveException(Exception):
+    pass
+
+
+class HistoricalDataTimeoutException(Exception):
+    pass

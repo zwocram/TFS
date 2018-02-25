1dbae7bc726365c3f10c2fcac056aacbb041cf08
Marco van der Zwan <mnzwan@xs4all.nl>
2018-02-25 15:17:05 +0100

    Added some lines of code to do some testing.

--- tfs/tfs.py
+++ tfs/tfs.py
@@ -1,12 +1,13 @@
 import configparser
-import time
 import pdb
 import sys
 import logging
 import decimal
 from optparse import OptionParser
 import pandas as pd
-from datetime import datetime, time
+# from datetime import datetime, time
+import datetime
+import time
 from utils import futures
 
 from utils.strategies import TFS, Unit
@@ -15,7 +16,7 @@ from db.database import Database
 from ib import ib
 from ibapi.contract import Contract
 
-EOD_TIME = time(22, 0)
+EOD_TIME = datetime.time(22, 0)
 
 if __name__ == '__main__':
     # Check that the port is the same as on the Gateway
@@ -42,20 +43,30 @@ if __name__ == '__main__':
     expiration_month_codes = expiration_dates[0]
     expiration_months = expiration_dates[1]
 
+    atr_horizon = int(config['tfs']['atr_horizon'])
+    entry_breakout_periods = int(config['tfs']['entry_breakout_periods'])
+    exit_breakout_periods = int(config['tfs']['exit_breakout_periods'])
+    account_risk = decimal.Decimal(config['tfs']['account_risk'])
+    unit_stop = int(config['tfs']['unit_stop'])
+    first_unit_stop = int(config['tfs']['first_unit_stop'])
+    nr_equities = int(config['tfs']['nr_equities'])
+    nr_units = int(config['tfs']['nr_units'])
+
     # parse arguments
     parser = OptionParser()
     parser.add_option("-e", "--eod", action="store_true", default=False,
                       dest="eod", help="Perform end of day actions.")
+    parser.add_option("-t", "--test", action="store_true", default=False,
+                      dest="test", help="Run program in test mode.")
     (options, args) = parser.parse_args()
     eod = options.eod
+    test_mode = options.test
 
     try:
         app = ib.IB("127.0.0.1", 4011, 10)
     except AttributeError as exp:
         print("Could not connect to the TWS API application.")
         sys.exit()
-
-    current_time = app.get_time()
 
     db = Database()
     tfs_strat = TFS()
@@ -66,73 +77,179 @@ if __name__ == '__main__':
     account_size = float([a[3] for a in account_info
                           if a[2] == 'NetLiquidation'][0])
 
-    if eod:
-        eod_data = tfs_strat.eod_data(config.items('portfolio'), ib=app,
-                                      config=config, account_size=account_size)
+    current_time = app.get_time()
 
-        # store account numbers in database
-        date = eod_data.iloc[0, eod_data.columns.get_loc('date')]
-        # db.insert_account_numbers(date, account_size, buying_power)
+    minute_interval = 5
+    second_interval = 5
+    eod_job_started = None
 
-        # create report
-        # TODO make code to create reports
+    print("Waiting till markets are closed.")
 
-        print("=============================")
-        print("Account size: ", account_size)
-        print("=============================")
-        print("Daily recap:\n", eod_data)
-        shorts = eod_data.loc[eod_data['close'] < eod_data['55DayLow']]
-        if shorts.shape[0] > 0:
+    """ONLY EXECUTE BELOW CODE IF TESTING
+    temp_time = datetime.datetime.now().time()
+    EOD_TIME = datetime.time(temp_time.hour, temp_time.minute + 1)
+    print(EOD_TIME)
+    """
+
+    while(True):
+        time.sleep(0.2)  # prevent the CPU from going wild
+        curr_time = datetime.datetime.now().time()
+        date_today_iso = datetime.datetime.now().date().isoformat()
+
+        """
+        # BELOW IS TEST CODE
+        pdb.set_trace()
+        test_unit = Unit(account_size=10100, atr=.86,
+                         account_risk=0.02,
+                         unit_stop=2,
+                         first_unit_stop=1,
+                         nr_equities=6,
+                         nr_units=4,
+                         ticker='TBT', price=39,
+                         pos_type="long", first_unit=False)
+        max_unit_id = 1
+        pos_id = 2
+        pos_type = "long"
+        new_unit = db.create_unit(test_unit, max_unit_id + 1,
+                                  pos_id, pos_type)
+        position_info = db.get_position_size("TBT")
+        updated_pos = db.update_position(position_info=position_info)
+        """
+
+        if (curr_time > EOD_TIME and not eod_job_started) or test_mode:
+            print("Starting end of day process at {0}."
+                  "".format(datetime.datetime.now().time()))
+            eod_job_started = True
+
+            eod_data = tfs_strat.eod_data(config.items('portfolio'), ib=app,
+                                          config=config,
+                                          account_size=account_size)
+            print(eod_data)
+
+            # store account numbers in database
+            date = eod_data.iloc[0, eod_data.columns.get_loc('date')]
+            db.insert_account_numbers(date, account_size, buying_power)
+
+            # create report
+            # TODO make code to create reports
+
             print("=============================")
-            print("Potential short candidates:\n\n",
-                  shorts[['date', 'close', 'atr',
-                          'position_size', 'stp_short']])
-        longs = eod_data.loc[eod_data['close'] > eod_data['55DayHigh']]
-        if longs.shape[0] > 0:
+            print("Account size: ", account_size)
             print("=============================")
-            print("Potential long candidates:\n\n",
-                  longs[['date', 'close', 'atr',
-                         'position_size', 'stp_long']])
+            print("Daily recap:\n", eod_data)
+            shorts = eod_data.loc[eod_data['close'] < eod_data['55DayLow']]
+            if shorts.shape[0] > 0:
+                print("=============================")
+                print("Potential short candidates:\n\n",
+                      shorts[['date', 'close', 'atr',
+                              'position_size', 'stp_short']])
+            longs = eod_data.loc[eod_data['close'] > eod_data['55DayHigh']]
+            if longs.shape[0] > 0:
+                print("=============================")
+                print("Potential long candidates:\n\n",
+                      longs[['date', 'close', 'atr',
+                             'position_size', 'stp_long']])
 
-        print("\n=============================\n")
-        for index, row in eod_data.iterrows():
-            close_price = row['close']
-            lt_day_high = row['55DayHigh']
-            lt_day_low = row['55DayLow']
-            ticker = row['ticker']
-            pos_size_df = db.get_position_size(ticker)
-            if pos_size_df.shape[0] == 0:
-                if close_price > lt_day_high:
-                    print('ready to enter 1st long position '
-                          'for {0}'.format(ticker))
-                elif close_price < lt_day_low:
-                    print('ready to enter 1st short position '
-                          'for {0}'.format(ticker))
-                else:
-                    print('not ready to enter new position '
-                          'for {0}'.format(ticker))
-            elif pos_size_df.shape[0] > 0:
-                pos_size = pos_size_df.pos_size.min()
-                risk_exposure = pos_size_df.risk_exposure.min()
-                max_unit_id = pos_size_df.unit_id.max()
-                price_target = pos_size_df.next_price_target.max()
-                if pos_size < 0:
-                    pos_type = "short"
-                elif pos_size > 0:
-                    pos_type = "long"
-                if ((close_price > price_target and pos_type == "long") or
-                        (close_price < price_target and pos_type == "short")):
-                    if max_unit_id < max_units:
-                        print('add new unit for {0}'.format(ticker))
-                    elif max_unit_id == max_units:
-                        if risk_exposure > 0:
-                            print("move up stop for {0}".format(ticker))
+            print("\n=============================\n")
+            for index, row in eod_data.iterrows():
+                close_price = row['close']
+                lt_day_high = row['55DayHigh']
+                lt_day_low = row['55DayLow']
+                st_day_high = row['20DayHigh']
+                st_day_low = row['20DayLow']
+                ticker = row['ticker']
+                instrument_id = db.get_instrument_id(ticker).instr_id[0]
+                atr = row['atr']
+                position_info = db.get_position_size(ticker)
 
-                print('positing size {0}: {1}'.format(ticker, pos_size))
+                if position_info.shape[0] == 0:
+                    create_new_unit = False
+                    if close_price > lt_day_high:
+                        print('ready to enter 1st long position '
+                              'for {0}'.format(ticker))
+                        pos_type = "long"
+                        create_new_unit = True
+                    elif close_price < lt_day_low:
+                        print('ready to enter 1st short position '
+                              'for {0}'.format(ticker))
+                        pos_type = "short"
+                        create_new_unit = True
+                    else:
+                        print('not ready to enter new position '
+                              'for {0}'.format(ticker))
+
+                    if create_new_unit:
+                        new_position = db.create_position(instrument_id,
+                                                          date_today_iso)
+                        unit = Unit(account_size=account_size, atr=atr,
+                                    account_risk=account_risk,
+                                    unit_stop=unit_stop,
+                                    first_unit_stop=first_unit_stop,
+                                    nr_equities=nr_equities, nr_units=nr_units,
+                                    ticker=ticker, price=close_price,
+                                    pos_type=pos_type, first_unit=True)
+                        new_unit = db.create_unit(unit, 1, new_position,
+                                                  pos_type)
+                        position_info = db.get_position_size(ticker)
+                        updated_pos = db.update_position(
+                            position_info=position_info)
+
+                elif position_info.shape[0] > 0:
+                    pos_id = position_info.pos_id.min()
+                    pos_size = position_info.pos_size.min()
+                    risk_exposure = position_info.risk_exposure.min()
+                    max_unit_id = position_info.unit_id.max()
+                    price_target = position_info.next_price_target.max()
+                    stop_price = position_info.stop_price.max()
+                    if pos_size < 0:
+                        pos_type = "short"
+                    elif pos_size > 0:
+                        pos_type = "long"
+
+                    # check if we have to add units or move up stops
+                    if ((close_price > price_target and pos_type == "long") or
+                            (close_price < price_target
+                             and pos_type == "short")):
+                        if max_unit_id < max_units:
+                            print('add new unit for {0}'.format(ticker))
+                            unit = Unit(account_size=account_size, atr=atr,
+                                        account_risk=account_risk,
+                                        unit_stop=unit_stop,
+                                        first_unit_stop=first_unit_stop,
+                                        nr_equities=nr_equities,
+                                        nr_units=nr_units,
+                                        ticker=ticker, price=close_price,
+                                        pos_type=pos_type, first_unit=True)
+                            new_unit = db.create_unit(unit, max_unit_id + 1,
+                                                      pos_id, pos_type)
+                            position_info = db.get_position_size(ticker)
+                            updated_pos = db.update_position(
+                                position_info=position_info)
+                            # update position info
+                        elif max_unit_id == max_units:
+                            if risk_exposure > 0:
+                                print("move up stop for {0} and "
+                                      "set new stop level.".format(ticker))
+                                # calculate new stop, move it up only 1 ATR
+                                updated_pos = db.update_position(
+                                    position_info=position_info,
+                                    break_even=True)
+
+                    # check if 20 day high/low crossed the stop
+
+                    print('positing size {0}: {1}'.format(ticker, pos_size))
                 # 1) get target price of last unit ==> target_price
                 # if close_price > (or <) target_price:
 
+            """ONLY EXECUTE BELOW CODE IF TESTING
+            temp_time = datetime.datetime.now().time()
+            EOD_TIME = datetime.time(temp_time.hour, temp_time.minute + 1)
+            print(EOD_TIME)
+            """
+
             # n_pos_instrument = db.get_position_size(ticker)
+        elif curr_time < EOD_TIME:
+            eod_job_started = False
 
         # store stuff in Database
         # blabla

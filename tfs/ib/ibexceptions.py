5def5facfa39ac3c0f33310c73df42866ed2fbf2
Marco van der Zwan <mnzwan@xs4all.nl>
2018-04-15 15:41:49 +0200

    Added some exceptions.

--- tfs/ib/ibexceptions.py
+++ tfs/ib/ibexceptions.py
@@ -20,3 +20,19 @@ class HistoricalDataRetrieveException(Exception):
 
 class HistoricalDataTimeoutException(Exception):
     pass
+
+
+class GetAccountDataException(Exception):
+    pass
+
+
+class CheckInstrumentExistenceException(Exception):
+    pass
+
+
+class GetCategoryIDException(Exception):
+    pass
+
+
+class InsertNewInstrumentException(Exception):
+    pass
